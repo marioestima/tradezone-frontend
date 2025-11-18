1391bc51b032d38ea70805b66da4ac9fadef0edd
marioestima <marioestimahackz@gmail.com>
2025-11-19 00:44:36 +0100

    remove console.lo

--- src/services/api.ts
+++ src/services/api.ts
@@ -15,7 +15,6 @@ export const api = axios.create({
 // Interceptor para enviar token automaticamente
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  console.log(token);
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }

d10d66f9246b85e1c8fe5c71543bd1d8765b5d75
marioestima <marioestimahackz@gmail.com>
2025-11-19 02:48:08 +0100

    auth logs

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,20 +1,16 @@
 import axios from "axios";
 
-// URL da API
-const API_URL =
-  import.meta.env.VITE_API_URL || "https://tradezone-api-v1.onrender.com";
-
-// Instância do Axios
 export const api = axios.create({
-  baseURL: API_URL,
+  baseURL:
+    import.meta.env.VITE_API_URL || "https://tradezone-api-v1.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// Interceptor para enviar token automaticamente
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+  console.log(token)
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
